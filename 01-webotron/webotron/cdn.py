8cc8498df34a47d22e8c4de25b8ff77cc38ee628
Christoph Heinig <christoph@heinig.xyz>
2019-11-27 10:26:29 +0100

    missing saved file

--- 01-webotron/webotron/cdn.py
+++ 01-webotron/webotron/cdn.py
@@ -82,27 +82,3 @@ class DistributionManager:
             'Delay': 30,
             'MaxAttempts': 50
         })
-
-    def create_cf_domain_record(self, zone, domain_name, cf_domain):
-        """Create a domain record in zone for domain_name."""
-        return self.client.change_resource_record_sets(
-            HostedZoneId=zone['Id'],
-            ChangeBatch={
-                'Comment': 'Created by webotron',
-                'Changes': [
-                    {
-                        'Action': 'UPSERT',
-                        'ResourceRecordSet': {
-                            'Name': domain_name,
-                            'Type': 'A',
-                            'AliasTarget': {
-                                # Fixed value from documentation
-                                'HostedZoneId': 'Z2FDTNDATAQYW2',
-                                'DNSName': cf_domain,
-                                'EvaluateTargetHealth': False
-                            }
-                        }
-                    }
-                ]
-            }
-        )
